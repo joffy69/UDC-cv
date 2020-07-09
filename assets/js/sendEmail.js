a29dc8bdf5b9648a1505457fb7e7877af07a66a0
joffy69 <jcwgibbs@gmail.com>
2020-07-09 17:07:13 +0000

    remember the various elements of the emailjs template

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log("hello");
+function sendMail(contactForm){
+    emailjs.send("gmail", "test", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("Success", response);       
+         },
+         function(error){
+             console.log("Failed!", error);
+         });
+         return false;
+}
